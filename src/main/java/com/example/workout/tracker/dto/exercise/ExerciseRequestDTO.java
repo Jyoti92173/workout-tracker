15bdaabf152b688ef3d7df6a33f88aae66b7a145
jyoti92173 <Jyoti53913@gmail.com>
2026-04-26 14:32:43 +0530

    Updted ExerciseRequestDTO with improvements

--- src/main/java/com/example/workout/tracker/dto/exercise/ExerciseRequestDTO.java
+++ src/main/java/com/example/workout/tracker/dto/exercise/ExerciseRequestDTO.java
@@ -1,4 +1,26 @@
 package com.example.workout.tracker.dto.exercise;
 
+import com.example.workout.tracker.model.MuscleGroup;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class ExerciseRequestDTO {
+    @NotBlank
+    private String name;
+
+    private String description;
+
+    @NotNull
+    private String category;
+
+    @NotNull
+    private MuscleGroup muscleGroup;
 }

648859f1b3fa8f0b8b395d87425a21f93e19878d
jyoti92173 <Jyoti53913@gmail.com>
2026-04-26 14:49:12 +0530

    Added new classes and optimize code structure

--- src/main/java/com/example/workout/tracker/dto/user/UserRegisterDTO.java
+++ src/main/java/com/example/workout/tracker/dto/user/UserRegisterDTO.java
@@ -1,4 +1,24 @@
 package com.example.workout.tracker.dto.user;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class UserRegisterDTO {
+    @NotBlank
+    private String username;
+
+    @Email
+    private String email;
+
+    @NotBlank
+    private String password;
 }
--- src/main/java/com/example/workout/tracker/dto/user/UserResponseDTO.java
+++ src/main/java/com/example/workout/tracker/dto/user/UserResponseDTO.java
@@ -1,4 +1,17 @@
 package com.example.workout.tracker.dto.user;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserResponseDTO {
+    private Long id;
+    private String name;
+    private String email;
+    private String role;
 }

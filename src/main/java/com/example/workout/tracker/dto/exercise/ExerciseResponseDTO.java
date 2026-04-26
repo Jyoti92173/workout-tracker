1f118bf068d5c138713bb9d6de48398cec6e40c1
jyoti92173 <Jyoti53913@gmail.com>
2026-04-26 14:45:07 +0530

    Enhanced Exercise form validation

--- src/main/java/com/example/workout/tracker/dto/exercise/ExerciseResponseDTO.java
+++ src/main/java/com/example/workout/tracker/dto/exercise/ExerciseResponseDTO.java
@@ -1,4 +1,20 @@
 package com.example.workout.tracker.dto.exercise;
 
+import com.example.workout.tracker.model.MuscleGroup;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class ExerciseResponseDTO {
+    private Long id;
+    private String name;
+    private String description;
+    private String category;
+    private MuscleGroup muscleGroup;
+
 }

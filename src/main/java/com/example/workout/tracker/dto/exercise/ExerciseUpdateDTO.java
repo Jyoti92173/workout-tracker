1f126aab81980cdd971b1f98378e7bcd9ec300c2
jyoti92173 <Jyoti53913@gmail.com>
2026-04-26 14:46:46 +0530

    Refactored ExerciseUpdateDTO implementation

--- src/main/java/com/example/workout/tracker/dto/exercise/ExerciseUpdateDTO.java
+++ src/main/java/com/example/workout/tracker/dto/exercise/ExerciseUpdateDTO.java
@@ -1,4 +1,18 @@
 package com.example.workout.tracker.dto.exercise;
 
+import com.example.workout.tracker.model.MuscleGroup;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class ExerciseUpdateDTO {
+    private String name;
+    private String description;
+    private String category;
+    private MuscleGroup muscleGroup;
 }

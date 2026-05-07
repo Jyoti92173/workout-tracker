234707ae78f7c0d877bfbe18c720e8d10ea28bfb
jyoti92173 <Jyoti53913@gmail.com>
2026-05-07 16:51:06 +0530

    Implemented centralized exception handling with GlobalExceptionHandler

--- src/main/java/com/example/workout/tracker/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/example/workout/tracker/exceptions/GlobalExceptionHandler.java
@@ -9,10 +9,9 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
 
 
-
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<String> handleNotFound(ResourceNotFoundException ex) {
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
     }
 
     @ExceptionHandler(IllegalArgumentException.class)

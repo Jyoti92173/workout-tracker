714d5695121d3afda2c0512d789ede588e18bfe9
jyoti92173 <Jyoti53913@gmail.com>
2026-08-18 22:25:19 +0530

    Create User CRUD APIs.

--- src/main/java/com/example/workout/tracker/controller/UserController.java
+++ src/main/java/com/example/workout/tracker/controller/UserController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/users")
 @RequiredArgsConstructor
 public class UserController {
-
     private final UserService userService;
 
     // Get current user info

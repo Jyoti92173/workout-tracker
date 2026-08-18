af68e35685a78873410959b56dfa63c1cf2a86e0
jyoti92173 <Jyoti53913@gmail.com>
2026-08-18 22:29:03 +0530

    Create Workout CRUD APIs

--- src/main/java/com/example/workout/tracker/controller/WorkoutController.java
+++ src/main/java/com/example/workout/tracker/controller/WorkoutController.java
@@ -18,7 +18,6 @@ import java.util.List;
 @RequestMapping("/api/workouts")
 @RequiredArgsConstructor
 public class WorkoutController {
-
     private final WorkOutService workOutService;
 
     @PostMapping("/create")

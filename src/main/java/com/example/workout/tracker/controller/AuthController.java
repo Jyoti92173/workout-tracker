6b34661e806662eae3f5c04f4b8cae931d61d9ad
jyoti92173 <Jyoti53913@gmail.com>
2026-08-18 22:13:09 +0530

    Create Auth API for register and login

--- src/main/java/com/example/workout/tracker/controller/AuthController.java
+++ src/main/java/com/example/workout/tracker/controller/AuthController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
 public class AuthController {
+    
     private final UserService userService;
     private final AuthService authService;
 

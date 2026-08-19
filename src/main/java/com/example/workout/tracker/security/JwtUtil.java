35be7ba737bba5f1d5db8d5a14f9b62cb5ec26db
jyoti92173 <Jyoti53913@gmail.com>
2026-08-19 23:09:41 +0530

    add JwtUtil for token generation and validation

--- src/main/java/com/example/workout/tracker/security/JwtUtil.java
+++ src/main/java/com/example/workout/tracker/security/JwtUtil.java
@@ -57,7 +57,7 @@ public class JwtUtil {
         return extractClaim(token, Claims::getSubject);
     }
 
-    // 📌 Extract Expiration Date
+    // Extract Expiration Date
     public Date extractExpiration(String token) {
         return extractClaim(token, Claims::getExpiration);
     }

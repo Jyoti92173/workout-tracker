22968e0db442f567bee253e5b9a197184639efa2
jyoti92173 <Jyoti53913@gmail.com>
2026-08-19 23:10:20 +0530

    add JwtUtil for token generation and validation

--- src/main/java/com/example/workout/tracker/security/JwtUtil.java
+++ src/main/java/com/example/workout/tracker/security/JwtUtil.java
@@ -67,7 +67,7 @@ public class JwtUtil {
         return extractExpiration(token).before(new Date());
     }
 
-    // 🔁 Generic Claim Extractor
+    //  Generic Claim Extractor
     public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
         final Claims claims = extractAllClaims(token);
         return claimsResolver.apply(claims);

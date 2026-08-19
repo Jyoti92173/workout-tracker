8a7fb29c8675426615996e2c203fe0fc20cee64a
jyoti92173 <Jyoti53913@gmail.com>
2026-08-19 22:08:42 +0530

    add ResourceNotFoundException for missing resources

--- src/main/java/com/example/workout/tracker/exceptions/ResourceNotFoundException.java
+++ src/main/java/com/example/workout/tracker/exceptions/ResourceNotFoundException.java
@@ -1,7 +1,9 @@
 package com.example.workout.tracker.exceptions;
 
 public class ResourceNotFoundException extends RuntimeException {
+
     public ResourceNotFoundException(String message) {
         super(message);
     }
+
 }

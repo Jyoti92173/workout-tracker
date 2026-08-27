f991170347aaddcdaf9a6ddabf565b4ad0c8bf57
jyoti92173 <Jyoti53913@gmail.com>
2026-08-27 23:15:24 +0530

    Add WorkoutUpdateDTO with Validation

--- src/main/java/com/example/workout/tracker/dto/workoutDTO/WorkoutUpdateDTO.java
+++ src/main/java/com/example/workout/tracker/dto/workoutDTO/WorkoutUpdateDTO.java
@@ -2,7 +2,7 @@ package com.example.workout.tracker.dto.workoutDTO;
 
 import java.util.List;
 
-public class WorkoutUpdateDTO {
+public class WorkoutUpdateDTO{
     private String workoutName;
     private String comment;
     private List<ExerciseUpdateDTO> exercises;

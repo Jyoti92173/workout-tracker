37eff0bb44655c33659a5681884c5f9a2bbb38e4
jyoti92173 <Jyoti53913@gmail.com>
2026-08-28 20:54:12 +0530

    add AdminUserService for admin user management

--- src/main/java/com/example/workout/tracker/service/AdminUserService.java
+++ src/main/java/com/example/workout/tracker/service/AdminUserService.java
@@ -5,7 +5,7 @@ import com.example.workout.tracker.dto.user.UserResponseDTO;
 
 import java.util.List;
 
-public interface AdminUserService {
+public interface AdminUserService{
     UserResponseDTO createUserByAdmin(UserRegisterDTO dto);
     List<UserResponseDTO> getAllUsers();
 }

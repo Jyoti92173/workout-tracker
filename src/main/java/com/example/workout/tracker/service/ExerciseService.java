2ac90d689cb81d12231b37d7d7c93d05be671d27
jyoti92173 <Jyoti53913@gmail.com>
2026-08-28 20:58:02 +0530

    add ExerciseService for exercise management

--- src/main/java/com/example/workout/tracker/service/ExerciseService.java
+++ src/main/java/com/example/workout/tracker/service/ExerciseService.java
@@ -9,7 +9,6 @@ import jakarta.validation.Valid;
 import java.util.List;
 
 public interface ExerciseService  {
-    
     ExerciseResponseDTO createExercise(@Valid ExerciseRequestDTO exerciseRequestDTO, String email);
     List<ExerciseResponseDTO> getAllExercises(String email);
     ExerciseResponseDTO getExerciseById(Long id);

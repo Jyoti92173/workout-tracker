412d2b7ee92f09f33761de882f154c32534b5188
jyoti92173 <Jyoti53913@gmail.com>
2026-08-28 21:01:55 +0530

    add UserService for user management

--- src/main/java/com/example/workout/tracker/service/UserService.java
+++ src/main/java/com/example/workout/tracker/service/UserService.java
@@ -8,7 +8,7 @@ import com.example.workout.tracker.dto.user.UserUpdateDTO;
 import java.util.List;
 
 public interface UserService {
-    
+
     UserResponseDTO createUser(UserRegisterDTO dto);
     UserResponseDTO createUserByAdmin(UserRegisterDTO dto);
     List<UserResponseDTO> getAllUsers();
@@ -20,6 +20,5 @@ public interface UserService {
 
 
 
-
-
+    
 }

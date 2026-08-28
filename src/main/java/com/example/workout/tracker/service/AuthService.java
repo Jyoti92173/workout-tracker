2f8a9543827fe702bc9d129f0595fa6374475f70
jyoti92173 <Jyoti53913@gmail.com>
2026-08-28 20:55:52 +0530

    add AuthService for user registration and login

--- src/main/java/com/example/workout/tracker/service/AuthService.java
+++ src/main/java/com/example/workout/tracker/service/AuthService.java
@@ -5,7 +5,7 @@ import com.example.workout.tracker.dto.auth.AuthResponseDTO;
 import com.example.workout.tracker.dto.auth.LoginRequestDTO;
 import com.example.workout.tracker.dto.auth.RegisterRequestDTO;
 
-public interface AuthService {
+public interface AuthService{
     AuthResponseDTO register(RegisterRequestDTO request);
     AuthResponseDTO login(LoginRequestDTO loginRequest);
 }
